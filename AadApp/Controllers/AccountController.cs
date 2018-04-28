9cfe386bcd465527acc2415b7d9cc8fd176123e6
twyrwas <tomasz.wyrwas@gmail.com>
2018-04-28 19:23:16 +0200

    Add empty account controller and basic views

---
+++ AadApp/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AadApp.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(string email)
+        {
+            //do stuff
+            return Redirect("/");
+        }
+
+        [HttpGet]
+        public ActionResult UserDetails()
+        {
+            return View();
+        }
+
+
+    }
+}
