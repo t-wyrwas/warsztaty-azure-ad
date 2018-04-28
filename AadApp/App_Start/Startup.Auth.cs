3f893c348c99333384b33aac2808e1381c4b0074
twyrwas <tomasz.wyrwas@gmail.com>
2018-04-28 20:21:43 +0200

    Add ADAL nuget and empty startup configuration classes

---
+++ AadApp/App_Start/Startup.Auth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace AadApp
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+        }
+    }
+}
---
+++ AadApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Owin;
+
+namespace AadApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
