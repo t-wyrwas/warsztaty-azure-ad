aa605d090ee69a86f676ee828fdfc0e78f62f14f
twyrwas <tomasz.wyrwas@gmail.com>
2018-04-29 11:54:14 +0200

    Implement authentication with AAD and OIDC

--- AadApp/App_Start/Startup.Auth.cs
+++ AadApp/App_Start/Startup.Auth.cs
@@ -1,16 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.Threading.Tasks;
 using System.Web;
 using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.IdentityModel.Claims;
+using Microsoft.Owin.Security.Notifications;
 
 namespace AadApp
 {
     public partial class Startup
     {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        public static readonly string Authority = aadInstance + tenantId;
+
+        // This is the resource ID of the AAD Graph API.  We'll need this to request a token to call the Graph API.
+        string graphResourceId = "https://graph.windows.net";
+
         public void ConfigureAuth(IAppBuilder app)
         {
+            // Configure: default sign in, cookie middleware, OIDC protocol middleware
 
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = Authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    AuthenticationMode = AuthenticationMode.Active,
+
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        // If there is a code in the OpenID Connect response, redeem it for an access token and refresh token, and store those away.
+                        AuthorizationCodeReceived = async (context) =>
+                        {
+                            await AuthCodeReceivedCallback(context, graphResourceId);
+                        }
+                    }
+                });
+        }
+
+        private static async Task<AuthenticationResult>  AuthCodeReceivedCallback(AuthorizationCodeReceivedNotification context, string graphResourceId)
+        {
+            var code = context.Code;
+            ClientCredential credential = new ClientCredential(clientId, appKey);
+            string signedInUserID = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            AuthenticationContext authContext = new AuthenticationContext(Authority);
+            return await authContext.AcquireTokenByAuthorizationCodeAsync(
+            code, new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)), credential, graphResourceId);
         }
     }
 }
