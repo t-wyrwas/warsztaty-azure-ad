fc41f95e7c6a15bf3e87ecbefde1ba932577609b
twyrwas <tomasz.wyrwas@gmail.com>
2018-05-07 23:44:17 +0200

    Add graph api client nuget and basic code for graph api functionality

--- AadApp/Controllers/AccountController.cs
+++ AadApp/Controllers/AccountController.cs
@@ -1,7 +1,12 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
 using System.Web;
 using System.Web.Mvc;
 using System.IdentityModel.Claims;
+using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security;
 using AadApp.ViewModels;
@@ -10,6 +15,8 @@ namespace AadApp.Controllers
 {
     public class AccountController : Controller
     {
+        private string graphResourceID = "https://graph.windows.net"; // AAD Graph, nie Microsoft Graph
+
         public void Login()
         {
             //todo
@@ -38,6 +45,26 @@ namespace AadApp.Controllers
             }
 
             return View();
+        }
+
+        public async Task<string> GetTokenForApplication()
+        {
+            var principal = HttpContext.GetOwinContext().Authentication.User;
+
+            var signedInUserID = principal.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var userObjectID = principal.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+            var clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+            var clientSecret = ConfigurationManager.AppSettings["ida:ClientSecret"];
+            var aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+            var tenantID = ConfigurationManager.AppSettings["ida:TenantId"];
+
+            // get a token for the Graph without triggering any user interaction (from the cache, via multi-resource refresh token, etc)
+            ClientCredential clientcred = new ClientCredential(clientId, clientSecret);
+            // initialize AuthenticationContext with the token cache of the currently signed in user, as kept in the app's database
+            AuthenticationContext authenticationContext = new AuthenticationContext(aadInstance + tenantID);
+            //AuthenticationResult authenticationResult = await authenticationContext.AcquireTokenSilentAsync(graphResourceID, clientcred, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
+            AuthenticationResult authenticationResult = await authenticationContext.AcquireTokenAsync(graphResourceID, clientcred);
+            return authenticationResult.AccessToken;
         }
     }
 }
